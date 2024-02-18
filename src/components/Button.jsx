0a00d39682d0a3cbe4f4f716ff6a18b75b0b14a5
Solomon Evoh <solomon.evoh@gmail.com>
2024-02-18 20:20:38 +0100

    Initial Commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Button = ({
+  label,
+  iconURL,
+  backgroundcolor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
+  return (
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundcolor
+          ? `${backgroundcolor} ${textColor} ${borderColor} `
+          : "bg-coral-red  text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}"}`}
+    >
+      {label}
+      {iconURL && (
+        <img src={iconURL} alt="" className="ml-2 rounded-full w-5 h-5" />
+      )}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  PopularProducts,
+  Footer,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  CustomerReviews,
+};
